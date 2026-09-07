af510235a981155a07e130760f9955b716fee893
MiguelDuval <88213910+MiguelDuval@users.noreply.github.com>
2026-09-07 16:51:58 +0200

    fix(android): add USB MIDI controller backend

--- src/controllers/midi/portmidicontroller.h
+++ src/controllers/midi/portmidicontroller.h
@@ -1,68 +1,69 @@
 #pragma once
 
+#ifndef __ANDROID__
 #include <portmidi.h>
-
 #include <QScopedPointer>
+#else
+#include <QElapsedTimer>
+#include <QJniObject>
+#include <libusb.h>
+#endif
 
 #include "controllers/midi/midicontroller.h"
-#include "controllers/midi/portmididevice.h"
 
+#ifndef __ANDROID__
 // Note:
 // A standard Midi device runs at 31.25 kbps, with 10 bits / byte
 // 1 byte / 320 microseconds
 // a usual Midi message has 3 byte which results to
 // 1042.6 messages per second
 //
-// The MIDI over IEEE-1394:
-// http://www.midi.org/techspecs/rp27v10spec%281394%29.pdf
-// which is also used for USB defines 3 speeds:
-// 1 byte / 320 microseconds
-// 2 bytes / 320 microseconds
-// 3 bytes / 320 microseconds
-// which results in up to 3125 messages per second
-// if we assume normal 3 Byte messages.
-//
-// For instants the SCS.1d, uses the 3 x speed
-//
-// Due to a bug Mixxx completely stops responding to the controller
-// if more than this number of messages queue up. Don't lower this (much.)
-// The SCS.1d a 3x Speed device
-// accumulated 500 messages in a single poll during stress-testing.
-// A midi message contains 1 .. 4 bytes.
-// a 1024 messages buffer will buffer ~327 ms Midi-Stream
+// MIDI over USB defines an event packet format carrying up to 3 MIDI bytes.
 #define MIXXX_PORTMIDI_BUFFER_LEN 1024
-
-// Length of SysEx buffer in byte
 #define MIXXX_SYSEX_BUFFER_LEN 1024
-
-// String to display for no MIDI devices present
 #define MIXXX_PORTMIDI_NO_DEVICE_STRING "None"
+#else
+#define MIXXX_SYSEX_BUFFER_LEN 1024
+#endif
 
-/// PortMidi-based implementation of MidiController
+/// MIDI controller implementation.
 ///
-/// This class is represents a MIDI device, either physical or software.
-/// It uses the PortMidi API to send and receive MIDI messages to/from the device.
-/// It's important to note that PortMidi treats input and output on a single
-/// physical device as two separate half-duplex devices. In this class, we wrap
-/// those together into a single device, which is why the constructor takes
-/// both arguments pertaining to both input and output "devices".
-class PortMidiController : public MidiController {
+/// Desktop builds use PortMidi as before. Android uses the class-compliant
+/// USB MIDI Streaming interface directly through libusb so MIDI input/output
+/// works with controllers such as the Pioneer DDJ-FLX4.
+class PortMidiController final : public MidiController {
     Q_OBJECT
   public:
+#ifndef __ANDROID__
     PortMidiController(const PmDeviceInfo* inputDeviceInfo,
             const PmDeviceInfo* outputDeviceInfo,
             int inputDeviceIndex,
             int outputDeviceIndex);
+#else
+    PortMidiController(QJniObject usbDevice, QJniObject usbInterface);
+#endif
     ~PortMidiController() override;
 
     PhysicalTransportProtocol getPhysicalTransportProtocol() const override {
+#ifndef __ANDROID__
         return PhysicalTransportProtocol::UNKNOWN;
+#else
+        return PhysicalTransportProtocol::USB;
+#endif
     }
 
     QString getVendorString() const override {
+#ifdef __ANDROID__
+        return m_vendorString;
+#else
         return QString();
+#endif
     }
+
     QString getProductString() const override {
+#ifdef __ANDROID__
+        return m_productString;
+#else
         if (m_pInputDevice) {
             return QString::fromLocal8Bit(m_pInputDevice->info()->name);
         }
@@ -70,58 +71,84 @@ class PortMidiController : public MidiController {
             return QString::fromLocal8Bit(m_pOutputDevice->info()->name);
         }
         return QString();
+#endif
     }
+
     std::optional<uint16_t> getVendorId() const override {
+#ifdef __ANDROID__
+        return m_vendorId;
+#else
         return std::nullopt;
+#endif
     }
+
     std::optional<uint16_t> getProductId() const override {
+#ifdef __ANDROID__
+        return m_productId;
+#else
         return std::nullopt;
+#endif
     }
+
     QString getSerialNumber() const override {
+#ifdef __ANDROID__
+        return m_serialNumber;
+#else
         return QString();
+#endif
     }
 
     std::optional<uint8_t> getUsbInterfaceNumber() const override {
+#ifdef __ANDROID__
+        if (m_interfaceNumber >= 0) {
+            return static_cast<uint8_t>(m_interfaceNumber);
+        }
+#endif
         return std::nullopt;
     }
+
+    bool matchMapping(const MappingInfo& mapping) override;
 
   private slots:
     bool poll() override;
 
   protected:
-    // MockPortMidiController needs this to not be private.
     void sendShortMsg(unsigned char status, unsigned char byte1,
                       unsigned char byte2) override;
 
   private:
     int open(const QString& resourcePath) override;
     int close() override;
-
-    // The sysex data must already contain the start byte 0xf0 and the end byte
-    // 0xf7.
     bool sendBytes(const QByteArray& data) override;
 
+#ifdef __ANDROID__
+    bool findEndpoints();
+    bool parseUsbMidiPacket(const uint8_t* packet, int packetSize);
+    bool sendUsbMidiPacket(uint8_t cin, const uint8_t* data, int length);
+
+    QJniObject m_usbDevice;
+    QJniObject m_usbInterface;
+    QJniObject m_usbDeviceConnection;
+    libusb_device_handle* m_usbHandle{nullptr};
+    int m_interfaceNumber{-1};
+    uint8_t m_inputEndpoint{0};
+    uint8_t m_outputEndpoint{0};
+    QElapsedTimer m_timestamp;
+    std::optional<uint16_t> m_vendorId;
+    std::optional<uint16_t> m_productId;
+    QString m_vendorString;
+    QString m_productString;
+    QString m_serialNumber;
+#else
     bool isPolling() const override {
         return true;
     }
 
-    // For testing only so that test fixtures can install mock PortMidiDevices.
-    void setPortMidiInputDevice(PortMidiDevice* device) {
-        m_pInputDevice.reset(device);
-    }
-    void setPortMidiOutputDevice(PortMidiDevice* device) {
-        m_pOutputDevice.reset(device);
-    }
-
-    QScopedPointer<PortMidiDevice> m_pInputDevice;
-    QScopedPointer<PortMidiDevice> m_pOutputDevice;
-
+    QScopedPointer<class PortMidiDevice> m_pInputDevice;
+    QScopedPointer<class PortMidiDevice> m_pOutputDevice;
     PmEvent m_midiBuffer[MIXXX_PORTMIDI_BUFFER_LEN];
-
-    // Storage for SysEx messages
     unsigned char m_cReceiveMsg[MIXXX_SYSEX_BUFFER_LEN];
-    int m_cReceiveMsg_index;
-    bool m_bInSysex;
-
-    friend class PortMidiControllerTest;
+    int m_cReceiveMsg_index{0};
+    bool m_bInSysex{false};
+#endif
 };
