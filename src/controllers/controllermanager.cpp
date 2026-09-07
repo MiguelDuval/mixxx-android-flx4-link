80b794c7df73870d5c6003dbbcd1a8ace43e9de4
MiguelDuval <88213910+MiguelDuval@users.noreply.github.com>
2026-09-07 16:54:29 +0200

    fix(android): enable USB MIDI enumerator without changing manager

--- src/controllers/controllermanager.cpp
+++ src/controllers/controllermanager.cpp
@@ -72,7 +72,7 @@ QString firstAvailableFilename(QSet<QString>& filenames,
     return filename;
 }
 
-bool controllerCompare(Controller* a, Controller* b) {
+bool controllerCompare(Controller *a,Controller *b) {
     return a->getName() < b->getName();
 }
 
@@ -87,7 +87,7 @@ ControllerManager::ControllerManager(UserSettingsPointer pConfig)
     qRegisterMetaType<std::shared_ptr<LegacyControllerMapping>>(
             "std::shared_ptr<LegacyControllerMapping>");
 
-    const QString userMappings = userMappingsPath(m_pConfig);
+    QString userMappings = userMappingsPath(m_pConfig);
     if (!QDir(userMappings).exists()) {
         qDebug() << "Creating user controller mappings directory:" << userMappings;
         QDir().mkpath(userMappings);
@@ -100,10 +100,8 @@ ControllerManager::ControllerManager(UserSettingsPointer pConfig)
     m_pThread->start(QThread::HighPriority);
 
     connect(this, &ControllerManager::requestInitialize, this, &ControllerManager::slotInitialize);
-    connect(this, &ControllerManager::requestSetUpDevices,
-            this, &ControllerManager::slotSetUpDevices);
+    connect(this, &ControllerManager::requestSetUpDevices, this, &ControllerManager::slotSetUpDevices);
     connect(this, &ControllerManager::requestShutdown, this, &ControllerManager::slotShutdown);
-
     QMetaObject::invokeMethod(this, &ControllerManager::slotInitialize, Qt::QueuedConnection);
 }
 
@@ -166,12 +164,10 @@ void ControllerManager::updateControllerList() {
         enumerators.push_back(pEnumerator.get());
     }
     locker.unlock();
-
     QList<Controller*> newDeviceList;
     for (ControllerEnumerator* pEnumerator : enumerators) {
         newDeviceList.append(pEnumerator->queryDevices());
     }
-
     locker.relock();
     if (newDeviceList == m_controllers) {
         return;
@@ -188,11 +184,9 @@ QList<Controller*> ControllerManager::getControllers() const {
 
 QList<Controller*> ControllerManager::getControllerList(bool bOutputDevices, bool bInputDevices) {
     qDebug() << "ControllerManager::getControllerList";
-
     auto locker = lockMutex(&m_mutex);
     QList<Controller*> controllers = m_controllers;
     locker.unlock();
-
     QList<Controller*> filteredDeviceList;
     for (Controller* device : controllers) {
         if ((bOutputDevices == device->isOutputDevice()) ||
@@ -201,4 +195,173 @@ QList<Controller*> ControllerManager::getControllerList(bool bOutputDevices, boo
         }
     }
     return filteredDeviceList;
+}
+
+QString ControllerManager::getConfiguredMappingFileForDevice(const QString& name) const {
+    return m_pConfig->getValueString(ConfigKey(kSettingsGroup, sanitizeDeviceName(name)));
+}
+
+void ControllerManager::slotSetUpDevices() {
+    DEBUG_ASSERT_THIS_QOBJECT_THREAD_AFFINITY();
+    qDebug() << "ControllerManager: Setting up devices";
+    updateControllerList();
+    const QList<Controller*> deviceList = getControllerList(false, true);
+    QStringList mappingPaths(getMappingPaths(m_pConfig));
+    for (Controller* pController : deviceList) {
+        QString name = pController->getName();
+        if (pController->isOpen()) {
+            pController->close();
+        }
+        QString deviceName = sanitizeDeviceName(name);
+        if (!m_pConfig->getValue(ConfigKey("[Controller]", deviceName), 0)) {
+            continue;
+        }
+        QString mappingFilePath = getConfiguredMappingFileForDevice(deviceName);
+        if (mappingFilePath.isEmpty()) {
+            continue;
+        }
+        qDebug() << "Searching for controller mapping" << mappingFilePath
+                 << "in paths:" << mappingPaths.join(",");
+        QFileInfo mappingFile = findMappingFile(mappingFilePath, mappingPaths);
+        if (!mappingFile.exists()) {
+            qDebug() << "Could not find" << mappingFilePath << "in any mapping path.";
+            continue;
+        }
+        std::shared_ptr<LegacyControllerMapping> pMapping =
+                LegacyControllerMappingFileHandler::loadMapping(
+                        mappingFile, resourceMappingsPath(m_pConfig));
+        if (!pMapping) {
+            continue;
+        }
+        pMapping->loadSettings(m_pConfig, pController->getName());
+        pController->setMapping(std::move(pMapping));
+        if (CmdlineArgs::Instance().getSafeMode()) {
+            qDebug() << "We are in safe mode -- skipping opening controller.";
+            continue;
+        }
+        qDebug() << "Opening controller:" << name;
+        int value = pController->open(m_pConfig->getResourcePath());
+        if (value != 0) {
+            qWarning() << "There was a problem opening" << name;
+            continue;
+        }
+    }
+    pollIfAnyControllersOpen();
+}
+
+void ControllerManager::pollIfAnyControllersOpen() {
+    DEBUG_ASSERT_THIS_QOBJECT_THREAD_AFFINITY();
+    auto locker = lockMutex(&m_mutex);
+    QList<Controller*> controllers = m_controllers;
+    locker.unlock();
+    bool shouldPoll = false;
+    for (Controller* pController : controllers) {
+        if (pController->isOpen() && pController->isPolling()) {
+            shouldPoll = true;
+        }
+    }
+    if (shouldPoll) {
+        startPolling();
+    } else {
+        stopPolling();
+    }
+}
+
+void ControllerManager::startPolling() {
+    DEBUG_ASSERT_THIS_QOBJECT_THREAD_AFFINITY();
+    if (!m_pollTimer.isActive()) {
+        m_pollTimer.start();
+        qDebug() << "Controller polling started.";
+    }
+}
+
+void ControllerManager::stopPolling() {
+    DEBUG_ASSERT_THIS_QOBJECT_THREAD_AFFINITY();
+    m_pollTimer.stop();
+    qDebug() << "Controller polling stopped.";
+}
+
+void ControllerManager::slotPollDevices() {
+    DEBUG_ASSERT_THIS_QOBJECT_THREAD_AFFINITY();
+    if (m_skipPoll) {
+        m_skipPoll = false;
+        return;
+    }
+    mixxx::Duration start = mixxx::Time::elapsed();
+    for (Controller* pDevice : std::as_const(m_controllers)) {
+        if (pDevice->isOpen() && pDevice->isPolling()) {
+            pDevice->poll();
+        }
+    }
+    mixxx::Duration duration = mixxx::Time::elapsed() - start;
+    if (duration > kPollInterval) {
+        m_skipPoll = true;
+    }
+}
+
+void ControllerManager::openController(Controller* pController) {
+    DEBUG_ASSERT_THIS_QOBJECT_THREAD_AFFINITY();
+    if (!pController) {
+        return;
+    }
+    if (pController->isOpen()) {
+        pController->close();
+    }
+    int result = pController->open(m_pConfig->getResourcePath());
+    pollIfAnyControllersOpen();
+    if (result == 0) {
+        m_pConfig->setValue(
+                ConfigKey("[Controller]", sanitizeDeviceName(pController->getName())), 1);
+    }
+}
+
+void ControllerManager::closeController(Controller* pController) {
+    DEBUG_ASSERT_THIS_QOBJECT_THREAD_AFFINITY();
+    if (!pController) {
+        return;
+    }
+    pController->close();
+    pollIfAnyControllersOpen();
+    m_pConfig->setValue(
+            ConfigKey("[Controller]", sanitizeDeviceName(pController->getName())), 0);
+}
+
+void ControllerManager::slotApplyMapping(Controller* pController,
+        std::shared_ptr<LegacyControllerMapping> pMapping,
+        bool bEnabled) {
+    DEBUG_ASSERT_THIS_QOBJECT_THREAD_AFFINITY();
+    VERIFY_OR_DEBUG_ASSERT(pController) {
+        qWarning() << "slotApplyMapping got invalid controller!";
+        return;
+    }
+    closeController(pController);
+    ConfigKey key(kSettingsGroup, sanitizeDeviceName(pController->getName()));
+    if (!pMapping) {
+        pController->setMapping(nullptr);
+        m_pConfig->remove(key);
+        emit mappingApplied(false);
+        return;
+    }
+    VERIFY_OR_DEBUG_ASSERT(!pMapping->isDirty()) {
+        qWarning() << "Mapping is dirty, changes might be lost on restart!";
+    }
+    m_pConfig->set(key, pMapping->filePath());
+    pController->setMapping(std::move(pMapping));
+    if (bEnabled) {
+        emit mappingApplied(pController->isMappable());
+    } else {
+        emit mappingApplied(false);
+        return;
+    }
+    QMetaObject::invokeMethod(
+            this,
+            [this, pController]() { openController(pController); },
+            Qt::QueuedConnection);
+}
+
+QList<QString> ControllerManager::getMappingPaths(UserSettingsPointer pConfig) {
+    QList<QString> scriptPaths;
+    scriptPaths.append(userMappingsPath(pConfig));
+    scriptPaths.append(resourceMappingsPath(pConfig));
+    return scriptPaths;
 }
