82d1ac6d333e71c948e541fc9da995a4dbcb8433
MiguelDuval <88213910+MiguelDuval@users.noreply.github.com>
2026-09-08 22:47:31 +0200

    fix: restore valid Android buildenv checksum

--- tools/android_buildenv.sh
+++ tools/android_buildenv.sh
@@ -39,7 +39,7 @@ if [ "$HOST_ARCH" == "x86_64" ]; then
         VCPKG_TARGET_TRIPLET="arm64-android"
         BUILDENV_BRANCH="2.7"
         BUILDENV_NAME="mixxx-deps-2.7-arm64-android-1c20f84a"
-        BUILDENV_SHA256="942bc2d3548948a872499dfd374deccafd73e1c247c7c1714e61c177975c4795"
+        BUILDENV_SHA256="6786c09f491166352880984842ffa6f554e974f30a3766a2392298444c1be83c"
     fi
 else
     echo "ERROR: Unsupported architecture detected: $HOST_ARCH"
